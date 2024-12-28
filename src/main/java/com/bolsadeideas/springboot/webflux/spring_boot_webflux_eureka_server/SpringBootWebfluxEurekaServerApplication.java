5a82091cda117e1f9b205a79e1a880255bd65a34
Jhon Condor <jhon_elen@outlook.es>
2024-12-28 12:06:23 -0500

    configure eureka server

--- src/main/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux_eureka_server/SpringBootWebfluxEurekaServerApplication.java
+++ src/main/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux_eureka_server/SpringBootWebfluxEurekaServerApplication.java
@@ -2,7 +2,9 @@ package com.bolsadeideas.springboot.webflux.spring_boot_webflux_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringBootWebfluxEurekaServerApplication {
 
