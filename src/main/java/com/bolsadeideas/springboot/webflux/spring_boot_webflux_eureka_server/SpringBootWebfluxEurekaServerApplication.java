f1addb6050bb789f431623204e4999cd550c2a1a
Jhon Condor <jhon_elen@outlook.es>
2024-12-28 08:25:35 -0500

    first commit

---
+++ src/main/java/com/bolsadeideas/springboot/webflux/spring_boot_webflux_eureka_server/SpringBootWebfluxEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.webflux.spring_boot_webflux_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebfluxEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebfluxEurekaServerApplication.class, args);
+	}
+
+}
